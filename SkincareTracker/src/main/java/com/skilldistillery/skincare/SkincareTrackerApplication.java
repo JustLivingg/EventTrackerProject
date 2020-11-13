96d7489f989b2c1a93425e6215ea27b65c40ecc7
JustLiving <JustinTLivingston@gmail.com>
2020-11-13 15:41:54 -0800

    Stub boot project with JPA project

---
+++ SkincareTracker/src/main/java/com/skilldistillery/skincare/SkincareTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.skincare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkincareTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkincareTrackerApplication.class, args);
+	}
+
+}
---
+++ SkincareTracker/src/test/java/com/skilldistillery/skincare/SkincareTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.skincare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkincareTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
