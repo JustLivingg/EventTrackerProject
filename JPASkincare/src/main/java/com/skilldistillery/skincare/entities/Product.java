ad9d013141f6133f079e0b889df568f2b375a540
JustLiving <JustinTLivingston@gmail.com>
2020-11-13 14:54:24 -0800

    Added entities and Junit test cases

---
+++ JPASkincare/src/main/java/com/skilldistillery/skincare/entities/Product.java
@@ -0,0 +1,75 @@
+package com.skilldistillery.skincare.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String brand;
+	
+	private String type;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", brand=" + brand + ", type=" + type + "]";
+	}
+
+	public Product() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+}
---
+++ JPASkincare/src/test/java/com/skilldistillery/skincare/entities/ProductTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.skincare.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Product product;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("SkincarePU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		product = em.find(Product.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		product = null;
+	}
+	
+	@Test
+	void test() {
+		assertNotNull(product);
+		assertEquals("Naturium", product.getBrand());
+		assertEquals("Retinol", product.getType());
+	}
+
+}
